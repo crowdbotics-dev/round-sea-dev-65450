8ee66ae5fb526fc29979c66f1ad27ace0c823a8d
Crowdbotics <engineering@crowdbotics.com>
2023-02-23 01:44:24 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/round_sea_dev_65450/MainActivity.java
@@ -0,0 +1,15 @@
+package com.round_sea_dev_65450;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "round_sea_dev_65450";
+  }
+}
